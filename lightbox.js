0c972170b685dd3f2717a075006bb2c979be02e8
Viktor Atanasov <137435334+DixBeF@users.noreply.github.com>
2025-03-24 19:33:31 +0200

    Add files via upload

--- lightbox.js
+++ lightbox.js
@@ -9,3 +9,12 @@ function openLightbox(img) {
 function closeLightbox() {
     document.getElementById('lightbox').style.display = "none";
 }
+
+//Close when clicking outside the image
+window.onclick = function(event) {
+    const lightbox = document.getElementById('lightbox');
+    const lightboxImg = document.getElementById('lightbox-img');
+    if (event.target === lightbox && event.target !== lightboxImg) {
+        lightbox.style.display = "none";
+    }
+};
